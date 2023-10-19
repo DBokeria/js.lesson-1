cfd8de5820b7893e2645f9ac171730db5228a246
davit <davitbkeria@gmail.com>
2023-10-20 01:05:42 +0400

    home work

---
+++ script.js
@@ -0,0 +1,24 @@
+let number =100;
+
+if(number < 50 ){
+   console.log('<50');
+} else if (number > 20){
+    console.log('>20');
+} else {
+    console.log('error');
+}
+
+let firstName = "მარიამი"
+
+let result = firstName= "მარიმი"  ? "true" : "false"
+console.log(result);
+
+
+let masterName = "mariam"
+switch (masterName) {
+    case "mariam":
+         console.log("true")
+         break;
+    default:
+        console.log("false")     
+}
